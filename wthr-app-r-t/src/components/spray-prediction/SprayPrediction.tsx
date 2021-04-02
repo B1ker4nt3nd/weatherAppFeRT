4afae749e41d4839d088794437bf141987213609
Kornél Marecskó <B1ker4nt3nd>
2021-04-02 10:28:27 +0200

    display corrections

--- wthr-app-r-t/src/components/spray-prediction/SprayPrediction.tsx
+++ wthr-app-r-t/src/components/spray-prediction/SprayPrediction.tsx
@@ -17,45 +17,29 @@ import {
 import SprayTimes from '../spray-times/SprayTimes';
 
 function SprayPrediction() {
-  // const [choices, setChoices] = useState(['első', 'második', '3', '4']);
-
-  // function handleClick(i) {
-  //   const choices = this.state.choices.slice();
-  //   choices[i] = "selected";
-  //   this.setState({
-  //     choices: choices,
-  //   });
-  // }
-
-  // componentDidMount(
-  //   var test = new apis.SprayApi();
-  // );
-
-  // const [cities, setCities] = useState([] as CityDto[]);
   const [cities, setCities] = useState([] as CityDto[]);
   const [cityId, setselectedCityId] = useState(0);
   const [sprayTimesLoading, setsprayTimesLoading] = useState(false);
   const [sprayTimes, setSprayTimes] = useState([] as CanSprayTimeDto[]);
 
-  // let cities: CityDto[] = [];
-
   useEffect(() => {
-    // if (!cities) {
     new apis.CitiesApi()
       .apiCitiesGet({ addCanSprayTime: false } as ApiCitiesGetRequest)
       .then((cities: CityDto[]) => setCities(cities));
-    // console.log(JSON.stringify(result));
-    // }
-    console.log('mount it!');
-  }, []); // passing an empty array as second argument triggers the callback in useEffect only after the initial render thus replicating `componentDidMount` lifecycle behaviour
+  }, []);
+
+  function upperCaseStartChar(cityName: string | null | undefined) {
+    if (cityName) {
+      return cityName.charAt(0).toUpperCase() + cityName.slice(1);
+    }
+    return '';
+  }
 
   function handleCityChange(event: ChangeEvent<any>) {
     const selectedIndex = event.target.options.selectedIndex;
     const _cityId = event.target.options[selectedIndex].getAttribute(
       'data-key'
     );
-    // console.log(_cityId);
-    // setselectedCityId(-1);
     setsprayTimesLoading(true);
     console.log(cityId);
 
@@ -63,31 +47,20 @@ function SprayPrediction() {
       .apiCitiesIdGet({ id: _cityId } as ApiCitiesIdGetRequest)
       .then((_city: CityDto) => {
         setSprayTimes(_city.canSprayTimes || []);
-        // setselectedCityId(_cityId);
-        // console.log(cityId);
         setsprayTimesLoading(false);
       });
-    // console.log(JSON.stringify(event));
   }
 
   return (
     <div className="spray-prediction">
-      {/* <Game onClick={(i) => {setChoices([' ' +  i + ' ', ' ' +  i + ' ', ' ' +  i + ' ', ' ' +  i + ' '])}} choices = choices /> */}
       <div className="flex-container">
         <Jumbotron className="title-box">
-          <h1>Tevékenységekre fel!</h1>
+          <h1>Permetezésre fel!</h1>
           <p>
-            Időjárás előrejelző alkalmazás ami nem azt írja, hogy milyen idő
-            lesz, hanem, hogy milyen tevékenységekre alkalmas.
+            permetezés előrejelző alkalmazás ami nem azt írja, hogy milyen idő
+            lesz, hanem, hogy mikor lesz permetezésre alkalmas.
           </p>
         </Jumbotron>
-        {/* <div className="col-md-6 page-description flex-container">
-          <div className="item">
-            Időjárás előrejelző alkalmazás ami nem azt írja, hogy milyen idő
-            lesz, hanem, hogy milyen tevékenységekre alkalmas.
-          </div>
-        </div>
-        <div className="col-md-6 fill"></div> */}
       </div>
       {isCitiesLoading(cities) ? (
         <Spinner animation="border" role="status" variant="secondary">
@@ -98,13 +71,12 @@ function SprayPrediction() {
           <div className="item">Városok</div>
           <Form className="item">
             <Form.Group controlId="exampleForm.SelectCustom">
-              {/* <Form.Label>Custom select</Form.Label> */}
               <Form.Control as="select" custom onChange={handleCityChange}>
                 <option key={0}></option>
                 {cities.map((city: CityDto) => {
                   return (
                     <option key={city.id} data-key={city.id}>
-                      {city.name}
+                      {upperCaseStartChar(city.name)}
                     </option>
                   );
                 })}
@@ -131,10 +103,5 @@ function SprayPrediction() {
 function isCitiesLoading(cities: CityDto[]): boolean {
   return !cities.some((_) => _);
 }
-
-// function isSprayTimesLoading(cityId: number): boolean {
-//   console.log(`is spray time ${cityId}`);
-//   return cityId < 0;
-// }
 
 export default SprayPrediction;
